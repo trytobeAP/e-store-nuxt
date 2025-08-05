fd6bd435d3e64df200feff46f660e07edb5ebc1e
tryrobeap <novil.00220303@gmail.com>
2025-08-05 17:19:53 +0300

    fix task-6: убрал слеш из пути к favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,7 @@ export default defineNuxtConfig({
         {
           rel: "icon",
           type: "image/x-icon",
-          href: "/favicon.ico",
+          href: "favicon.ico",
         },
       ],
       script: [
